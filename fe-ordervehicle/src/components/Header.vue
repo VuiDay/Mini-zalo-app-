784b816997b9b070278de454cf744e4af566d958
VuiDay <cuongtran2003207@gmail.com>
2024-05-31 21:18:38 +0700

    update code

---
+++ fe-ordervehicle/src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="relative">
+      <img class="w-[100%]" src="@/assets/image/header.png" alt="" />
+    </div>
+    <div
+      class="flex justify-between absolute w-[100%] px-[20px]"
+      style="top: 45px"
+    >
+      <div>
+        <p
+          style="
+            color: #111;
+            font-family: 'Anton', sans-serif;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 20px;
+          "
+        >
+          Chào mừng trở lại,
+        </p>
+        <p
+          style="
+            color: #111;
+            font-family: 'Anton', sans-serif;
+            font-size: 16px;
+            font-style: normal;
+            font-weight: 600;
+            line-height: 20px;
+          "
+        >
+          Cường Trần
+        </p>
+      </div>
+      <div>
+        <Avatar
+          image="https://res.cloudinary.com/dn6xdmqbl/image/upload/v1716228175/avatar_user/es2wxaefwkqft9vhocpu.jpg"
+          shape="circle"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
