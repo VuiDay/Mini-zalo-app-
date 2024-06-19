1036011a7901bb68097b02d3aa930d03f100dd1f
VuiDay <cuongtran2003207@gmail.com>
2024-06-19 16:48:18 +0700

    update code

--- fe-main/src/stores/user.js
+++ fe-main/src/stores/user.js
@@ -19,7 +19,7 @@ export const useUserStore = defineStore("user", {
         avatar:
           "https://scontent.fvii2-4.fna.fbcdn.net/v/t39.30808-6/438304440_1150209179659623_6594686254167414971_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=bKDw-h0bZP8Q7kNvgFAfP1v&_nc_ht=scontent.fvii2-4.fna&oh=00_AYAAZ_GL7X70ZOjUGs37ueYp7EDaIFA46NxQ8HRdwcJ34g&oe=667826CF",
         followedOA: false,
-        id: 113113131313131,
+        id: "113113131313131",
         idByOA: true,
         isSensitive: false,
         name: "Cường Nè",
@@ -38,7 +38,8 @@ export const useUserStore = defineStore("user", {
       };
       const user = await axios.post(
         "https://be-mini-app.minhquancao0.workers.dev/api/user/user-current",
-        dataUser.phone ? dataUser : fakeData
+        // dataUser.phone ? dataUser : fakeData
+        dataUser
       );
       this.userInfor = user.data;
       console.log(this.userInfor);
