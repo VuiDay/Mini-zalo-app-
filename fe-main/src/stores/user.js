5f3f22a538e4fa88324b82eb7342c282ceaef6e2
VuiDay <cuongtran2003207@gmail.com>
2024-06-19 11:24:59 +0700

    update code

--- fe-main/src/stores/user.js
+++ fe-main/src/stores/user.js
@@ -28,9 +28,19 @@ export const useUserStore = defineStore("user", {
         ...this.data,
         phone: res.data.data?.number,
       };
+      const fakeData = {
+        avatar:
+          "https://scontent.fvii2-4.fna.fbcdn.net/v/t39.30808-6/438304440_1150209179659623_6594686254167414971_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=bKDw-h0bZP8Q7kNvgFAfP1v&_nc_ht=scontent.fvii2-4.fna&oh=00_AYAAZ_GL7X70ZOjUGs37ueYp7EDaIFA46NxQ8HRdwcJ34g&oe=667826CF",
+        followedOA: false,
+        id: 113113131313131,
+        idByOA: undefined,
+        isSensitive: false,
+        name: "Cường Nè",
+        phone: "84768921323",
+      };
       const user = await axios.post(
         "https://be-mini-app.minhquancao0.workers.dev/api/user/user-current",
-        dataUser
+        dataUser.phone ? dataUser : fakeData
       );
       console.log(user.data);
       this.userInfor = user.data;
